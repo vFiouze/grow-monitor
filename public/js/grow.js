0140139bc3e86b8d973cdfae207f2655fb77fd7f
Vincent Fuseau <vFiouze@MacBook-Pro-de-Vincent.local>
2019-11-19 00:07:50 -0800

    Update UI

--- public/js/grow.js
+++ public/js/grow.js
@@ -38,39 +38,11 @@ plotChart = function(data){
 		averageTempDataSet.push(averageTemp)
 	}
 	var tempselected=document.getElementById('temperature')
+	var chartData = buildChartData(labels,temperature,averageTempDataSet,tempselected,humidity,true,true)
 	var ctx = document.getElementById('myChart').getContext('2d');
 	var myChart = new Chart(ctx, {
 	    type: 'line',
-	    data:{
-	    	labels:labels,
-	    	datasets: [{
-	    		data : temperature,
-	    		label: "Temperature (°" + tempselected.options[tempselected.selectedIndex].value+')',
-	    		fill:false,
-	    		backgroundColor:'rgba(215, 39, 39, 1)',
-	    		borderColor:'rgba(215, 39, 39, 1)',
-	    		borderWidth:2,
-	    		yAxisID: 'temperature-y-axis',
-	    	},
-	    	{
-	    		data : averageTempDataSet,
-	    		label: "Average temperature (°" + tempselected.options[tempselected.selectedIndex].value+')',
-	    		fill:false,
-	    		backgroundColor:'rgba(242, 140, 7, 1)',
-	    		borderColor:'rgba(242, 140, 7, 1)',
-	    		borderWidth:2,
-	    		yAxisID: 'temperature-y-axis',
-	    	},
-	    	{
-	    		data : humidity,
-	    		label: "Humidity (%)",
-	    		fill:false,
-	    		backgroundColor:'rgba(32, 154, 248, 1)',
-	    		borderColor:'rgba(32, 154, 248, 1)',
-	    		borderWidth:2,
-	    		yAxisID: 'humidity-y-axis'
-	    	}]
-	    },
+	    data:chartData,
 	    options: {
         	scales: {
             	yAxes: [{
@@ -79,7 +51,9 @@ plotChart = function(data){
                 	position:'left',
                 	scaleLabel:{
                 		display:true,
-                		labelString:'Temperature (°' + tempselected.options[tempselected.selectedIndex].value+')'
+                		labelString:'Temperature (°' + tempselected.options[tempselected.selectedIndex].value+')',
+                		fontStyle:'bold',
+            			fontSize:'14'
                 	}
             	}, {
                 	id: 'humidity-y-axis',
@@ -88,13 +62,17 @@ plotChart = function(data){
                 	labelString:'Humidity (%)',
                 	scaleLabel:{
                 		display:true,
-                		labelString:'Humidity (%)'
+                		labelString:'Humidity (%)',
+                		fontStyle:'bold',
+            			fontSize:'14'
                 	}
             	}],
             	xAxes:[{
             		scaleLabel:{
             			display:true,
             			labelString:'Date (YYYY-MM-DD HH:mm:ss)',
+            			fontStyle:'bold',
+            			fontSize:'14'
             		},
             		ticks:{
             			autoSkip: true,
@@ -107,7 +85,45 @@ plotChart = function(data){
 	});
 	this.graph = myChart
 }
-
+buildChartData = function(labels,temperature,averageTempDataSet,tempselected,humidity,showTemp,showHumidity){
+	return {
+	    	labels:labels,
+	    	datasets: [{
+	    		data : temperature,
+	    		label: "Temperature (°" + tempselected.options[tempselected.selectedIndex].value+')',
+	    		fill:false,
+	    		backgroundColor:'rgba(215, 39, 39, 1)',
+	    		borderColor:'rgba(215, 39, 39, 1)',
+	    		borderWidth:1,
+	    		pointRadius:1,
+	    		pointBackgroundColor:'rgba(0, 0, 0, 1)',
+	    		yAxisID: 'temperature-y-axis',
+	    		showLine:showTemp
+	    	},
+	    	{
+	    		data : averageTempDataSet,
+	    		label: "Average temperature (°" + tempselected.options[tempselected.selectedIndex].value+')',
+	    		fill:false,
+	    		backgroundColor:'rgba(242, 140, 7, 1)',
+	    		borderColor:'rgba(242, 140, 7, 1)',
+	    		borderWidth:1,
+	    		pointRadius:0,
+	    		yAxisID: 'temperature-y-axis',
+	    		showLine:showTemp
+	    	},
+	    	{
+	    		data : humidity,
+	    		label: "Humidity (%)",
+	    		fill:false,
+	    		backgroundColor:'rgba(32, 154, 248, 1)',
+	    		borderColor:'rgba(32, 154, 248, 1)',
+	    		borderWidth:1,
+	    		pointRadius:1,
+	    		yAxisID: 'humidity-y-axis',
+	    		showLine:showHumidity
+	    	}]
+	    }
+}
 document.addEventListener('DOMContentLoaded', (event) => {
 	let from = document.getElementById('from')
 	let to = document.getElementById('to')
@@ -117,5 +133,45 @@ document.addEventListener('DOMContentLoaded', (event) => {
 	from.value =  nowMinus12Hours.format('YYYY-MM-D HH:mm:ss')
 	getresult()
 })
+var showTemp = function(){
+	var checkBoxTemp = document.getElementById('showtemp').checked
+	if(checkBoxTemp==true){
+		graph.data.datasets[0].showLine = true;
+		graph.data.datasets[1].showLine = true;
+		color = 'rgba(215, 39, 39, 1)'
+		graph.data.datasets[0].pointBackgroundColor=color
+		graph.data.datasets[1].pointBackgroundColor=color
+		graph.data.datasets[0].pointBorderColor=color
+		graph.data.datasets[1].pointBorderColor=color
+		
+	}else{
+		graph.data.datasets[0].showLine = false;
+		graph.data.datasets[1].showLine = false;
+		color = 'rgba(255,255,255,1)'
+		graph.data.datasets[0].pointBackgroundColor=color
+		graph.data.datasets[1].pointBackgroundColor=color
+		graph.data.datasets[0].pointBorderColor=color
+		graph.data.datasets[1].pointBorderColor=color
+		
+	}
+
+	graph.update()
+}
+var showHumid = function(){
+	var checkBoxTemp = document.getElementById('showhumid').checked
+	if(checkBoxTemp==true){
+		color = 'rgba(32, 154, 248, 1)',
+		graph.data.datasets[2].showLine = true;
+		graph.data.datasets[2].pointBorderColor=color
+		graph.data.datasets[2].pointBorderColor=color
+	}else{
+		color = 'rgba(255,255,255,1)'
+		graph.data.datasets[2].showLine = false;
+		graph.data.datasets[2].pointBorderColor=color
+		graph.data.datasets[2].pointBorderColor=color
+	}
+
+	graph.update()
+}
 
 var graph = null
